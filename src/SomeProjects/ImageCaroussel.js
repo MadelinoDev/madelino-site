69a46cba2dcef71852de2be9a8745674cb014c76
MadelinoDev <madelinodev@gmail.com>
2024-11-05 13:53:07 +0100

    Caroussel listo pero no está centrado horizontalmente
    Hay que darle estilos y coherencia

--- src/SomeProjects/ImageCaroussel.js
+++ src/SomeProjects/ImageCaroussel.js
@@ -1,46 +1,48 @@
 import React, { useState } from 'react';
 import SomeProjectsStyle from './SomeProjectsStyles.module.css'
 
-function ImageCarousel({images}) {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
- 
+function ImageCarousel({ images }) {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const handlePrevClick = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+
+    const handlePrevClick = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    const handleNextClick = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    return (
+        <div>
+            <div className={SomeProjectsStyle.carousel}>
+                <button className={SomeProjectsStyle.prevButton} onClick={handlePrevClick}>Anterior</button>
+
+                <img
+                    className={SomeProjectsStyle.prevImage}
+                    src={images[(currentImageIndex + images.length - 1) % images.length].src}
+                    alt="Imagen previa"
+                />
+                <img
+                    className={SomeProjectsStyle.activeImage}
+                    src={images[currentImageIndex].src}
+                    alt="Imagen activa"
+                />
+                <img
+                    className={SomeProjectsStyle.nextImage}
+                    src={images[(currentImageIndex + 1) % images.length].src}
+                    alt="Imagen siguiente"
+                />
+                <button className={SomeProjectsStyle.nextButton} onClick={handleNextClick}>Siguiente</button>
+            </div>
+            <h3 className={SomeProjectsStyle.imageTitle}>{images[currentImageIndex].title}</h3>
+            <div className={SomeProjectsStyle.imageText}>{images[currentImageIndex].text}</div>
+        </div>
     );
-  };
-
-  const handleNextClick = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  return (
-    <div className={SomeProjectsStyle.carousel}>
-      <button className={SomeProjectsStyle.prevButton} onClick={handlePrevClick}>Anterior</button>
-      <div className={SomeProjectsStyle.imageContainer}>
-        <img
-          className={SomeProjectsStyle.prevImage}
-          src={images[(currentImageIndex + images.length - 1) % images.length].src}
-          alt="Imagen previa"
-        />
-        <img
-          className={SomeProjectsStyle.activeImage}
-          src={images[currentImageIndex].src}
-          alt="Imagen activa"
-        />
-        <img
-          className={SomeProjectsStyle.nextImage}
-          src={images[(currentImageIndex + 1) % images.length].src}
-          alt="Imagen siguiente"
-        />
-      </div>
-      <button className={SomeProjectsStyle.nextButton} onClick={handleNextClick}>Siguiente</button>
-      <p className={SomeProjectsStyle.imageText}>{images[currentImageIndex].text}</p>
-    </div>
-  );
 }
 
 export default ImageCarousel;
--- src/SomeProjects/SomeProjects.js
+++ src/SomeProjects/SomeProjects.js
@@ -12,23 +12,49 @@ import SPOimg from '../img/Games/SPO.png'
 
 const images = [
     {
-      src: SOSimg,
-      text: 'Esta es la primera imagen',
+        src: SOSimg,
+        title: 'SUPERFIGHTERS OF SURVIVAL',
+        text: (<div >
+            <ul className={SomeProjectsStyle.listUnordered}>
+                <li>Superfighters of Survival es un proyecto de Unreal Engine 5 con un equipo de 30 personas divididas en los principales roles de la industria: diseño, programación, producción y arte.</li>
+                <li>SOS es un juego de combate a muerte de gladiadores donde los jugadores luchan por ganar el favor del público en una arena cambiante.</li>
+                <li>Este es el proyecto en el que he participado que más se acerca al trabajo real en la industria. El desafío de tener un equipo grande nos permite entender el flujo de trabajo de las empresas del sector.</li>
+                <li>
+                Trabajar en este proyecto me permitió desarrollar mis habilidades en la documentación de ideas, ya sea con Excel, diagramas o comunicación efectiva, y me permitió aprender algunas disciplinas dentro del diseño de juegos como:
+                    <ul>
+                        <li>IA: Diseño de diferentes IA con comportamientos cruzados.</li>
+                        <li>Combate: Aprendizaje sobre notificadores de eventos en animaciones, ajuste y pulido al máximo.</li>
+                        <li>UI/UX: Diseño e implementación extensos de características de UI como tutoriales, mecánicas, animaciones...</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>)
     },
     {
-      src: CBimg,
-      text: 'Esta es la segunda imagen',
+        src: CBimg,
+        title: 'CURSEBREAKER',
+        text: (<div>
+            <p>Cursebreaker es el segundo proyecto importante de Unity que completé durante mi año Erasmus y el primero en el que el diseño fue mi enfoque principal, aunque también contribuí a la programación y producción. El proyecto enfrentó numerosos contratiempos, lo que destacó las complejidades del desarrollo de videojuegos. Estos desafíos subrayaron la importancia de la producción y me ayudaron a desarrollar habilidades en mediación, gestión del tiempo y trabajo en equipo.</p>
+            <p>Decidimos crear un juego de realidad virtual con seguimiento de manos, una tecnología nueva con muy pocas referencias. Utilizamos bibliotecas de Unity para el reconocimiento de manos, el seguimiento de manos y el seguimiento de gestos, junto con OpenXR para los controles básicos de VR. La falta de referencias nos brindó la oportunidad de innovar e iterar sobre controles y diseños novedosos. Este proyecto profundizó significativamente mi comprensión de la experiencia del usuario, un campo que sigo explorando y aprendiendo.</p>
+            <p>El equipo estaba compuesto por siete especialistas, y la comunicación se llevó a cabo constantemente en inglés.</p>
+        </div>)
     },
     {
         src: SPOimg,
-        text: 'Esta es la tercera imagen',
-      },
+        title: 'STEAMPUNK OVERRIDE',
+        text: (<div >
+            <ul className={SomeProjectsStyle.listUnordered}>
+                <li>Juego desarrollado en una semana por 5 amigos utilizando Unity durante un game jam.</li>
+                <li>El tema era "De 0 a 100", y decidimos hacer un juego de fiesta donde la cooperación es esencial para arrancar un tren y llevarlo a 100 km/h mientras es atacado por monstruos.</li>  
+            </ul>
+        </div>)
+    },
     // Agrega más imágenes aquí
-  ];
+];
 
-export default function SomeProjects({title}){
-    return  (
-        <div className={SomeProjectsStyle.SomeProjects}>
+export default function SomeProjects({ title }) {
+    return (
+        <div id="proyectos"className={SomeProjectsStyle.SomeProjects}>
             <h2>{title}</h2>
             <ImageCarousel images={images}></ImageCarousel>
             {/* <ProjectImg src={SOSimg} gameName="SUPERFIGHTERS OF SURVIVAL"></ProjectImg>
